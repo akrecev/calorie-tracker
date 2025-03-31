d3fe00d62c166d2a2bace1b90675cb5588ed5503
akrecev <akrecev@gmail.com>
2025-03-31 20:49:21 +0300

    rename proteins at DishDto

--- src/main/java/com/kretsev/calorietracker/dto/DishDto.java
+++ src/main/java/com/kretsev/calorietracker/dto/DishDto.java
@@ -12,6 +12,6 @@ public record DishDto(
         Long id,
         @NotBlank(message = "Название блюда не должно быть пустым") String name,
         @PositiveOrZero(message = "Количество калорий не может быть отрицательным") int calories,
-        @PositiveOrZero(message = "Количество протеинов не может быть отрицательным") double proteins,
+        @PositiveOrZero(message = "Количество белков не может быть отрицательным") double proteins,
         @PositiveOrZero(message = "Количество жиров не может быть отрицательным") double fats,
         @PositiveOrZero(message = "Количество углеводов не может быть отрицательным") double carbs) {}
