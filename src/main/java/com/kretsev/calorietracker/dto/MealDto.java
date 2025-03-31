81231cee434cafb3425966b078951a0aa09aed0c
akrecev <akrecev@gmail.com>
2025-03-31 18:51:55 +0300

    spotless apply

--- src/main/java/com/kretsev/calorietracker/dto/MealDto.java
+++ src/main/java/com/kretsev/calorietracker/dto/MealDto.java
@@ -1,7 +1,6 @@
 package com.kretsev.calorietracker.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -9,5 +8,4 @@ import java.util.List;
  * DTO for {@link com.kretsev.calorietracker.model.Meal}
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record MealDto(Long id, UserDto user, LocalDateTime mealDate, List<DishDto> dishes) {
-}
+public record MealDto(Long id, UserDto user, LocalDateTime mealDate, List<DishDto> dishes) {}
--- src/main/java/com/kretsev/calorietracker/dto/UserDto.java
+++ src/main/java/com/kretsev/calorietracker/dto/UserDto.java
@@ -9,11 +9,12 @@ import jakarta.validation.constraints.*;
  * DTO for {@link User}
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record UserDto(Long id,
-                      @Size(message = "Имя должно содержать от 3 до 255 символов", min = 3, max = 255) String name,
-                      @Email(message = "Некорректный адрес электронной почты") String email,
-                      @Size(max = 150, message = "Возраст должен быть от 0 до 150 лет") int age,
-                      @Size(max = 350, message = "Вес должен быть от 0 до 350 кг") double weight,
-                      @Size(max = 250, message = "Рост должен быть от 0 до 250 см")  double height,
-                      Goal goal, int dailyCalorieNorm) {
-}
+public record UserDto(
+        Long id,
+        @Size(message = "Имя должно содержать от 3 до 255 символов", min = 3, max = 255) String name,
+        @Email(message = "Некорректный адрес электронной почты") String email,
+        @Size(max = 150, message = "Возраст должен быть от 0 до 150 лет") int age,
+        @Size(max = 350, message = "Вес должен быть от 0 до 350 кг") double weight,
+        @Size(max = 250, message = "Рост должен быть от 0 до 250 см") double height,
+        Goal goal,
+        int dailyCalorieNorm) {}
