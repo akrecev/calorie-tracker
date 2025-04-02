522fbe29e4e2b839078441db206f0d526705c237
akrecev <akrecev@gmail.com>
2025-04-02 09:56:37 +0300

    spotless apply

--- src/main/java/com/kretsev/calorietracker/dto/MealDto.java
+++ src/main/java/com/kretsev/calorietracker/dto/MealDto.java
@@ -3,7 +3,6 @@ package com.kretsev.calorietracker.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
--- src/main/java/com/kretsev/calorietracker/dto/UserDto.java
+++ src/main/java/com/kretsev/calorietracker/dto/UserDto.java
@@ -13,8 +13,14 @@ public record UserDto(
         Long id,
         @Size(message = "Имя должно содержать от 3 до 255 символов", min = 3, max = 255) String name,
         @Email(message = "Некорректный адрес электронной почты") String email,
-        @Min(value = 0L, message = "Возраст не может быть отрицательным") @Max(value = 150, message = "Возраст должен быть от 0 до 150 лет") int age,
-        @Min(value = 0L, message = "Вес не может быть отрицательным") @Max(value = 350L, message = "Вес должен быть от 0 до 350 кг") double weight,
-        @Min(value = 0L, message = "Рост не может быть отрицательным") @Max(value = 250L, message = "Рост должен быть от 0 до 250 см") double height,
+        @Min(value = 0L, message = "Возраст не может быть отрицательным")
+                @Max(value = 150, message = "Возраст должен быть от 0 до 150 лет")
+                int age,
+        @Min(value = 0L, message = "Вес не может быть отрицательным")
+                @Max(value = 350L, message = "Вес должен быть от 0 до 350 кг")
+                double weight,
+        @Min(value = 0L, message = "Рост не может быть отрицательным")
+                @Max(value = 250L, message = "Рост должен быть от 0 до 250 см")
+                double height,
         Goal goal,
         int dailyCalorieNorm) {}
